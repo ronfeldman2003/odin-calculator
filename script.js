bad2df0b92c9ea85d7c464ecef7aefebbe03b27e
ronfeldman <ronfeldman2003@gmail.com>
2023-05-02 15:49:58 +0300

    make number apper on screen when pressed

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 let number1 = 0;
-let number2 = 0;
+let number2 = 1;
 let operator = "";
+let main_display_text = "";
+let top_display_text = "";
+const main_display = document.getElementsByClassName('screen_now')[0];
+const top_display = document.getElementsByClassName('screen_prev')[0];
+const numberButtons = document.querySelectorAll(`button.number`);
+numberButtons.forEach(numberButton => numberButton.addEventListener('click',addNumToDisplay));
+const operatorButtons = document.querySelectorAll(`button.operator`);
+operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click',operatorPresed));
+const equalls = document.getElementById("btn_=");
+equalls.addEventListener('click',equalls_fun);
 
+function equalls_fun(e){
+    top_display_text=main_display_text;
+    top_display.textContent=top_display_text;
+    main_display_text = operate(number1,number2,operator);
+    main_display.textContent = main_display_text;
+}
 
+function addNumToDisplay(){
+    main_display_text += this.textContent;
+    main_display.textContent = main_display_text;
+}
+function operatorPresed(e){
+    console.log(e);
+    number1=main_display_text;
+    operator = this.textContent;
+    main_display_text += operator ;
+    main_display.textContent = main_display_text;
+}
 
 function add(a,b){
-    return a+b;
+    return parseFloat(a)+parseFloat(b);
 }
 function subtract(a,b){
     return a-b;
