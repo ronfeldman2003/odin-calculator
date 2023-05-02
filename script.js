616e21f158f1ff03e387ddfb72e4de6d3dd238dc
ronfeldman <ronfeldman2003@gmail.com>
2023-05-02 16:24:21 +0300

    added calculation script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let number1 = 0;
-let number2 = 1;
+let number2 = 0;
 let operator = "";
 let main_display_text = "";
 let top_display_text = "";
@@ -11,24 +11,48 @@ const operatorButtons = document.querySelectorAll(`button.operator`);
 operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click',operatorPresed));
 const equalls = document.getElementById("btn_=");
 equalls.addEventListener('click',equalls_fun);
+const clear = document.getElementById("btn_clear");
+clear.addEventListener('click',clear_fun);
 
-function equalls_fun(e){
-    top_display_text=main_display_text;
+
+function clear_fun(){ //reset calculator
+    number1 = 0 
+    number2 = 0
+    operator = ""
+    main_display_text = "0"
+    top_display_text = ""
+    updateDisplay();
+    main_display_text = ""
+}
+function updateDisplay(){ //update the display in the html
+    main_display.textContent = main_display_text
     top_display.textContent=top_display_text;
+}
+function equalls_fun(){ //display calculations
+    top_display_text=main_display_text; 
+    number2 = main_display_text.slice(main_display_text.indexOf(operator)+1);
+    console.log(`display text:${main_display_text} num1:${number1} num2:${number2} operator:${operator}`);
     main_display_text = operate(number1,number2,operator);
-    main_display.textContent = main_display_text;
+    updateDisplay();
+    number1 = 0 
+    number2 = 0
+    operator = ""
 }
 
-function addNumToDisplay(){
+function addNumToDisplay(){// add pressed numbers to display
     main_display_text += this.textContent;
-    main_display.textContent = main_display_text;
+    updateDisplay();
 }
-function operatorPresed(e){
+function operatorPresed(e){ // do the calculations
+    if(operator != ""){
+        equalls_fun();
+    }
     console.log(e);
     number1=main_display_text;
     operator = this.textContent;
     main_display_text += operator ;
-    main_display.textContent = main_display_text;
+    updateDisplay();
+    
 }
 
 function add(a,b){
@@ -56,7 +80,7 @@ function operate(n1,n2,o){
             return multiply(n1,n2);
             break;
         case "/":
-            if(n2 == 0) return "cant divide by zero";
+            if(n2 == 0) return "math eror";
             return divide(n1,n2);
             break;
         default:
